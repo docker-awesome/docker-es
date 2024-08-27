5e657729a868fa20dda76d439a57083821a45c70
Yoda <docker.cpu@qq.com>
2024-08-27 22:03:49 +0800

    fix(taro): loading

--- packages/taro/src/loading/index.ts
+++ packages/taro/src/loading/index.ts
@@ -1,26 +1,26 @@
-import Taro from "@tarojs/taro";
+import { hideLoading, showLoading } from '@tarojs/taro';
 
 class Loading {
   static #count = 0;
 
   static start(args?: { title?: string; mask?: boolean }) {
     Loading.#count++;
-    const options = typeof args === "object" ? args : {};
-    Taro.showLoading({
-      title: "加载中",
+    const options = typeof args === 'object' ? args : {};
+    showLoading({
+      title: '加载中',
       mask: true,
-      ...options,
+      ...options
     });
   }
 
   static end() {
     if (Loading.#count > 0) Loading.#count--;
-    if (Loading.#count === 0) Taro.hideLoading();
+    if (Loading.#count === 0) hideLoading();
   }
 
   static reset() {
     Loading.#count = 0;
-    Taro.hideLoading();
+    hideLoading();
   }
 }
 
