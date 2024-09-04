0b40b6e02446ceafd8b1aadeb1acd243738b2a78
Yoda <docker.cpu@qq.com>
2024-09-04 21:37:40 +0800

    refactor(getter): clone, uuid

--- packages/getter/src/index.d.ts
+++ packages/getter/src/index.d.ts
@@ -11,11 +11,13 @@ export type DataType = PrimitiveType | CompositeType;
 
 export default class Getter {
   static count: (data: ArrayFromParameter) => number;
-  static uuid: () => string;
+  static id(options?: { prefix?: string; suffix?: string }): string;
+  static uuid: () => `${string}-${string}-${string}-${string}-${string}`;
   static type: (data: any) => DataType;
   static round: (number: number, precision?: number) => number;
   static random: (from: number, to: number, integer?: boolean) => number;
-  static clone<T>(data: T, isAsync?: false): T;
-  static clone<T>(data: T, isAsync: true): Promise<T>;
+  // static clone<T>(data: T, isAsync?: false): T;
+  // static clone<T>(data: T, isAsync: true): Promise<T>;
+  static clone<T>(data: T): T;
   static jsonp: (url: string, params?: Record<string, any>) => Promise<any>;
 }
--- packages/getter/src/index.ts
+++ packages/getter/src/index.ts
@@ -5,8 +5,13 @@ class Getter {
     return Array.from(data).length;
   }
 
-  static uuid(): string {
-    return Math.random().toString(36).slice(2);
+  static id(options?: { prefix?: string; suffix?: string }): string {
+    const { prefix = '', suffix = '' } = options || {};
+    return `${prefix}${Math.random().toString(36).slice(2)}${suffix}`;
+  }
+
+  static uuid() {
+    return crypto.randomUUID();
   }
 
   static type(data: any): DataType {
@@ -29,8 +34,8 @@ class Getter {
       : Math.random() * (max - min) + min;
   }
 
-  static clone(data: any, isAsync = false) {
-    if (isAsync) {
+  static clone(data: any /* , isAsync = false */) {
+    /* if (isAsync) {
       return new Promise((resolve, reject) => {
         try {
           const { port1, port2 } = new MessageChannel();
@@ -42,13 +47,19 @@ class Getter {
           reject(error);
         }
       });
-    }
+    } */
 
     const objectMap = new Map();
 
     const deepClone = (value: any) => {
+      if (value instanceof RegExp) return new RegExp(value);
+      if (value instanceof Date) return new Date(value);
+      if (value instanceof Map) return new Map(value);
+      if (value instanceof Set) return new Set(value);
+
       const type = typeof value;
       if (type !== 'object' || type === null) return value;
+
       if (objectMap.has(value)) {
         return objectMap.get(value);
       }
